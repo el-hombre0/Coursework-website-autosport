ae0310cc5744f957983ef65cfb97ee44ee004253
el-hombre0 <evendot@yandex.ru>
2021-11-10 14:01:44 +0300

    master

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-$(window).scroll(function(){
-    var docscroll=$(document).scrollTop();
-    if(docscroll>$(window).height()){
-      $('nav').css({'height': $('nav').height(),'width': $('nav').width()}).addClass('fixed');
-    }else{
-      $('nav').removeClass('fixed');
+// $(window).scroll(function(){
+//     var docscroll=$(document).scrollTop();
+//     if(docscroll>$(window).height()){
+//       $('nav').css({'height': $('nav').height(),'width': $('nav').width()}).addClass('fixed');
+//     }else{
+//       $('nav').removeClass('fixed');
+//     }
+//   });
+$(function() {
+  let header = $('.header');
+   
+  $(window).scroll(function() {
+    if($(this).scrollTop() > 230) {
+     header.addClass('header_fixed');
+    } else {
+     header.removeClass('header_fixed');
     }
   });
+});
