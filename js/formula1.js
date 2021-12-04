ef24739ea303b0cd8314e88ee936f095b4f37aa6
Stanislav Efimtsev <stasef@hotmail.com>
2021-12-05 02:43:35 +0300

    master

--- js/formula1.js
+++ js/formula1.js
@@ -1,6 +1,6 @@
-$(document).ready(function(){
-    $('.header__burger').click(function(event){ //обращаемся к классу header__burger и добавляем ему событие click
-        $('.header__burger,.header__menu').toggleClass('active') //бургер шапке и бургер меню добавить класс active (toggle - нажали и зафиксировался класс)
-        $('body').toggleClass('lock');//добавляем к body класс lock, чтобы блокировать прокрутку страницы во время открытого меню
-    });
-});
+// $(document).ready(function(){
+//     $('.header__burger').click(function(event){ //обращаемся к классу header__burger и добавляем ему событие click
+//         $('.header__burger,.header__menu').toggleClass('active') //бургер шапке и бургер меню добавить класс active (toggle - нажали и зафиксировался класс)
+//         $('body').toggleClass('lock');//добавляем к body класс lock, чтобы блокировать прокрутку страницы во время открытого меню
+//     });
+// });
